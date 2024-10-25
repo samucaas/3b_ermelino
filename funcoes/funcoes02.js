cf9280cef45b9f25270f072161c48e39086f32be
samucaas <samuel.medeiros.silva16@escola.pr.gov.br>
2024-10-25 19:36:39 +0000

    criar fuçã nomeada e anônima

---
+++ funcoes/funcoes02.js
@@ -0,0 +1,11 @@
+function logar(usuario, senha) {
+    alert('logado com afuncoa nomeada!')
+    alert('ola, ' + usuario)
+}
+
+logar("samuel",1056)
+
+const conectar = function (usuario, senha) {
+    alert('logado com afuncoa nomeada!')
+    alert('ola, ' + usuario)
+}
